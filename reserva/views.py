896d183865bbdbbe101d9669854604a81b44a91a
Alvaro Ian <alvaro-ian@hotmail.com>
2019-06-15 22:04:01 -0300

    Adicionada pagina reserva_recurso.html e o forms de reserva

--- reserva/views.py
+++ reserva/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ReservarForm
 
 # Create your views here.
+def reservar_recurso(request):
+	if request.method == 'POST':
+		form = ReservarForm(request.POST)
+		if form.is_valid():
+			identificador = form.cleaned_data['identificador']
+			return render(request, 'reserva_recurso.html', {'form': form})
+
+	return render(request, 'reserva_recurso.html')
