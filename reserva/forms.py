4def860eb1347557a08ddb0a8fbaf0fddc810b1f
Alvaro Ian <alvaro-ian@hotmail.com>
2019-06-16 13:26:40 -0300

    Ajustei o form de reservar e o reserva_recurso.html

--- reserva/forms.py
+++ reserva/forms.py
@@ -1,4 +1,9 @@
+from django.forms import ModelForm
 from django import forms
+from .models import Reserva
+from .widgets import BootstrapDateTimePickerInput
 
-class ReservarForm(forms.Form):
-	identificador = forms.CharField(max_length=10)
+class ReservarForm(ModelForm):
+	class Meta:
+		model = Reserva
+		fields = ['identificador', 'recurso', 'usuario', 'data_reserva', 'tempo_alocacao']
--- reserva/views.py
+++ reserva/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import ReservarForm
+from .models import Reserva
+from datetime import date
 
 # Create your views here.
 def reservar_recurso(request):
-	if request.method == 'POST':
-		form = ReservarForm(request.POST)
-		if form.is_valid():
-			identificador = form.cleaned_data['identificador']
-			return render(request, 'reserva_recurso.html', {'form': form})
+	form = ReservarForm(request.POST, None)
 
-	return render(request, 'reserva_recurso.html')
+	if form.is_valid():
+		idt = form.cleaned_data['identificador']
+		rec = form.cleaned_data['recurso']
+		usu = form.cleaned_data['usuario']
+		data_r = form.cleaned_data['data_reserva']
+		tempo_a = form.cleaned_data['tempo_alocacao']
+
+		reserva = Reserva(
+				identificador=idt,
+				recurso=rec,
+				usuario=usu,
+				data_solicitacao=date.today(),
+				data_reserva=data_r,
+				tempo_alocacao=tempo_a
+			)
+
+		try:
+			reserva.save()
+			print('Reserva ' + reserva.__str__() + ' salva com sucesso')
+		except:
+			print('Ocorreu um erro ao salvar a reserva')
+		
+	return render(request, 'reserva_recurso.html', {'form': form})
