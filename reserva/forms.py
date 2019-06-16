f9afaf3abdb89b9a374ea45d7e2d515d9c02d283
Alvaro Ian <alvaro-ian@hotmail.com>
2019-06-16 16:25:45 -0300

    Adicionada validacao da data de reserva na reserva de recurso

--- reserva/forms.py
+++ reserva/forms.py
@@ -2,6 +2,8 @@ from django.forms import ModelForm
 from django import forms
 from .models import Reserva
 from recurso.models import Recurso
+from datetime import datetime, timedelta
+from pytz import utc
 
 class ReservarForm(ModelForm):
 	class Meta:
@@ -12,10 +14,21 @@ class ReservarForm(ModelForm):
 		cleaned_data = super().clean()
 		print(cleaned_data.get('tempo_alocacao'))
 
-		tma_reserva = cleaned_data.get('tempo_alocacao')
+		# Validacao da data de reserva (nao pode ser menor antes do horario atual)
+		data_r = cleaned_data.get('data_reserva')
 		recurso = cleaned_data.get('recurso')
+		if data_r is not None and recurso is not None:
+			tmr = datetime.now() + timedelta(hours=recurso.TMR)
+			tmr = utc.localize(tmr)
+			if data_r < tmr:
+				msg = 'Reservas apenas a partir de ' + tmr.strftime('%d/%m/%y, %H:%M') + '.'
+				self.add_error('data_reserva', msg)
+
+		# Validacao do tempo maximo de alocacao
+		tma_reserva = cleaned_data.get('tempo_alocacao')
 		if recurso is not None:
 			if tma_reserva > recurso.TMA:
-				self.add_error('tempo_alocacao', 'Tempo de alocação não pode exceder ' + str(recurso.TMA) + ' horas.')
+				msg = 'Tempo de alocação não pode exceder ' + str(recurso.TMA) + ' horas.'
+				self.add_error('tempo_alocacao', msg)
 
 		return cleaned_data
