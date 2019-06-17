f5a1b8a84e6d7f505517200b04aeb7f949522480
Alvaro Ian <alvaro-ian@hotmail.com>
2019-06-17 12:43:13 -0300

    Atualizar home page

--- reserva/models.py
+++ reserva/models.py
@@ -8,7 +8,7 @@ class Reserva(models.Model):
 	recurso = models.ForeignKey(Recurso, on_delete=models.DO_NOTHING)
 	usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 	data_solicitacao = models.DateTimeField('Solicitado em', auto_now_add=True)	# auto_now_add - acho que faz sentido ser assim
-	data_reserva = models.DateTimeField('Reservado para')
+	data_reserva = models.DateTimeField('Data e hora para reserva')
 	tempo_alocacao = models.PositiveIntegerField('Tempo de alocação') # limitado pelo tma do recurso reservado
 
 	objects = models.Manager()
